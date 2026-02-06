7b7a6357821497c7b518363e12a1fe89217fcaf8
Sourav Dey <souravdeybca053@gmail.com>
2026-02-06 12:57:55 +0530

    configure tailwind setup

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+// 1st layer(env config)
 const dotenv = require('dotenv');
 dotenv.config();
 
+// 2nd layer(database config)
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DB)
 
+// 3rd layer(server create)
 const express = require('express');
 const app = express();
+app.listen(process.env.PORT || 8080)
+
+// 4th layer (middlewares)
+app.use(express.static('view'))
